2d26b9b13ad900667f849c08e94b1f076dc1d821
DELL <DELL@DESKTOP-F4D14VO>
2019-11-25 16:00:52 +0800

    第二次提交

--- src/cn/hh/love.java
+++ src/cn/hh/love.java
@@ -4,7 +4,9 @@ public class love {
 
 	public static void main(String[] args) {
 		System.out.println("666");
-		//hh
+		System.out.println("66666");
+		System.out.println("66666666");
+		//第二次
 	}
 
 }
